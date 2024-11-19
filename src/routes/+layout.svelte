fabec1ba4ecc41b37660ac98f18b44ba942ad1bf
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-11-20 05:11:29 +0700

    add bootstrap

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<link
+		href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/cyborg/bootstrap.min.css"
+		rel="stylesheet"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.css"
+	/>
+</svelte:head>
+
+<slot />
