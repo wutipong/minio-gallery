e3c3a1b22113f33d9c2a4e6701c35db55a38b693
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-11-21 04:10:41 +0700

    update the proxy on server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { handler } from './build/handler.js';
 import express from 'express';
-import 'dotenv/config'
+import 'dotenv/config';
 import proxy from 'express-http-proxy';
 
 const app = express();
@@ -10,15 +10,18 @@ app.get('/healthcheck', (req, res) => {
 	res.end('ok');
 });
 
-app.use('/s3', proxy(process.env.MINIO_ENDPOINT, {
-    proxyReqPathResolver: (req) => req.url.replace(/^\/s3/, ''),
-    proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
-        proxyReqOpts.setHeader('X-Minio-Extract', 'true');
-		proxyReqOpts.removeHeader("authorization");
+app.use(
+	'/s3',
+	proxy(process.env.MINIO_ENDPOINT, {
+		proxyReqPathResolver: (req) => req.url.replace(/^\/s3/, `/${process.env.MINIO_BUCKET}/`),
+		proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
+			proxyReqOpts.headers['X-Minio-Extract'] = 'true';
+			// proxyReqOpts.removeHeader('authorization');
 
-        return proxyReqOpts;
-      }
-}));
+			return proxyReqOpts;
+		}
+	})
+);
 
 // let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
